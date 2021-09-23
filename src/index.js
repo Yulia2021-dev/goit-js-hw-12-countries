b757f03d578133d6bab9f720d3c23d310a0af3e7
Yulia <ulaboyko@ukr.net>
2021-09-23 16:50:20 +0300

    add new file country-list

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import './sass/main.scss';
 import './js/fetchCountries';
+import './js/api-access';
+import './js/get-refs';
---
+++ src/js/api-access.js
@@ -0,0 +1,5 @@
+const BASE_URL = 'https://restcountries.eu/rest/v2';
+
+function fetchCountry(countryName) {
+  return fetch(`${BASE_URL}/name/${countryName}`).then(response => response.json());
+}
